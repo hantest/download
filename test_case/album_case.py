1ef7b5dbe2b4c078a4505b64dc0d97688a29de21
unknown <Administrator@hnn1.mdm.com.local>
2018-05-29 19:22:53 +0800

    专辑管理

--- test_case/album_case.py
+++ test_case/album_case.py
@@ -9,19 +9,20 @@ import unittest, sys
 sys.path.append("./modles")
 sys.path.append("./test_page")
 from models import myunit
-from test_page.collect_page import CollectPage
+from test_page.album_page import AlbumPage
 
 #
-# 用例：收藏资源
-# time:2018-4-26
+# 用例：专辑管理
+# time：2018-5-29
 # @HAN
 #
-class Collect(myunit.MyTest):
+
+class Album(myunit.MyTest):
 
 
-	def test_collect(self):
-		''' 资源收藏 '''
-		CollectPage(self.driver).collect_page()
+	def test_album(self):
+		''' 专辑管理 '''
+		AlbumPage(self.driver).album_page()
 
 	def tearDown(self):
 		self.driver.quit()
